a083e59e35271f66bb077425c038f603f6f28fdc
djhi <gildas.garcia@gmail.com>
2014-10-15 08:54:29 +0200

    Update mailgun.sh
    missing escape characters

--- extra/mailgun.sh
+++ extra/mailgun.sh
@@ -3,5 +3,5 @@
 echo "-----> Adding MailGun config -> MAIL_URL env"
 cat > "$APP_CHECKOUT_DIR"/.profile.d/mailgun.sh <<EOF
   #!/bin/sh
-  export MAIL_URL="${MAIL_URL:-smtp://${MAILGUN_SMTP_LOGIN}:${MAILGUN_SMTP_PASSWORD}@${MAILGUN_SMTP_SERVER}:${MAILGUN_SMTP_PORT}}";
+  export MAIL_URL="\${MAIL_URL:-smtp://\${MAILGUN_SMTP_LOGIN}:\${MAILGUN_SMTP_PASSWORD}@\${MAILGUN_SMTP_SERVER}:\${MAILGUN_SMTP_PORT}}";
 EOF
